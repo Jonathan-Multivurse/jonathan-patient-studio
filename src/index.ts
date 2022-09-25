e0ff18bd363468280183ab19c16e8440fbda39c4
Jonathan Daniels <multivurse.technologies@gmail.com>
2022-09-24 18:25:57 -0700

    improved code

--- src/index.ts
+++ src/index.ts
@@ -17,21 +17,11 @@ export async function returnSiteTitles() {
     'https://www.neowin.net/'
   ]
 
-  const titles = []
+  const texts: string[] = await Promise.all(urls.map(url => 
+    fetch(url, { method: 'GET' }).then(res => res.text())
+  ))
 
-  for (const url of urls) {
-    const response = await fetch(url, { method: 'GET' })
-
-    if (response.status === 200) {
-      const data = await response.text()
-      const match = data.match(/<title>(.*?)<\/title>/)
-      if (match?.length) {
-        titles.push(match[1])
-      }
-    }
-  }
-
-  return titles
+  return texts.map(text => (text.match(/<title>(.*?)<\/title>/) || [])[1])
 }
 
 /**
@@ -43,32 +33,24 @@ export async function returnSiteTitles() {
  * @param localData array of objects
  * @returns array of objects
  */
+
 export function findTagCounts(localData: Array<SampleDateRecord>): Array<TagCounts> {
-  const tagCounts: Array<TagCounts> = []
-
-  for (let i = 0; i < localData.length; i++) {
-    const tags = localData[i].tags
-
-    for (let j = 0; j < tags.length; j++) {
-      const tag = tags[j]
-
-      for (let k = 0; k < tagCounts.length; k++) {
-        if (tagCounts[k].tag === tag) {
-          tagCounts[k].count++
-        } else {
-          tagCounts.push({ tag, count: 1 })
-        }
-      }
+  const tagCountsObj = localData.reduce((result: { [index: string]: number }, item) => {
+    for (let tag of item.tags) {
+      result[tag] = result[tag] || 0
+      result[tag]++
     }
-  }
+    return result
+  }, {})
 
-  return tagCounts
+  return Object.entries(tagCountsObj).map(([tag, count]): TagCounts => ({ tag, count }))
 }
 
 /**
  * Calcualte total price
  *
- * Task: Write a function that reads in data from `importedItems` array (which is imported above) and calculates the total price, including taxes based on each
+ * Task: Write a function that reads in data from `importedItems` array (which is imported above) and calculates
+ * the total price, including taxes based on each
  * countries tax rate.
  *
  * Here are some useful formulas and infomration:
@@ -77,7 +59,22 @@ export function findTagCounts(localData: Array<SampleDateRecord>): Array<TagCoun
  *  - the "importTaxRate" is based on they destiantion country
  *  - if the imported item is on the "category exceptions" list, then no tax rate applies
  */
+
 export function calcualteImportCost(importedItems: Array<ImportedItem>): Array<ImportCostOutput> {
   // please write your code in here.
   // note that `taxRate` has already been imported for you
+
+  const res = importedItems.map(item => {
+    const { name, quantity, unitPrice, countryDestination } = item
+   
+    const taxObject: ImportTaxRate | undefined = taxRates.find(r => r.country === countryDestination)
+    const tax = taxObject?.categoryExceptions.includes(item.category) ? 0 : taxObject?.importTaxRate || 0
+   
+    const subtotal = unitPrice * quantity
+    const importCost = subtotal * tax
+    const totalCost = importCost + subtotal
+
+    return { name, subtotal, importCost, totalCost }
+  })
+  return res;
 }
